f9267536f4322a3b9bf1e8e59d0cdf664aa3c1c0
Mintnoii <MintnoiiQing@gmail.com>
2022-11-03 23:46:17 +0800

    feat: styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
 import {getDatabase, getPage } from '@/lib/notion'
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await getDatabase('')
-  console.log('data', data)
-  return {
-    props:{
-      data
-    }
-  }
-  // const { articles, tags } = convertToArticleList(data)
-
-  // const featuredArticle = articles[0]
-
-  // return {
-  //   props: {
-  //     featuredArticle,
-  //     articles: articles.slice(0),
-  //     tags,
-  //   },
-  //   revalidate: 30,
-  // }
-}
-
+import '@/styles/global.css'
 
  export default function Home() {
   return <main><h1>Hello, Next.js!</h1></main>
