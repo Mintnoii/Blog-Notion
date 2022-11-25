9cc176a4cada1498f45d80e1bcd6eee0fb16f6de
Mintnoii <MintnoiiQing@gmail.com>
2022-11-25 11:28:14 +0800

    wip: blogIds

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,19 +3,24 @@ import {getBlogs,getWorkSpace } from '@/lib/notion'
 import '@/styles/global.css'
 import Link from 'next/link'
 import { ArticleList } from '@/components/blog/ArticleList'
+import { IArticle } from '@/lib/types'
 
- export default async function PostsPage() {
-  const articles = await getBlogs('2b33b4bab64d4aeb9ee8f62aabe1ae5d')
-  console.log(articles,'articles')
+export default async function PostsPage() {
+  const blogsIds:string[] = JSON.parse(process.env.NOTION_BLOGS_IDS || '[]')
+  const promiseArr = blogsIds.map((id:string) => getBlogs(id))
+  const results = await Promise.allSettled(promiseArr)
+  const articles = results.map((result:PromiseSettledResult<IArticle[]>) => result.status === 'fulfilled' ? result.value : []).flat()
+   // blogs 即为所有博客数据组成的数组
+    console.log(results ,'results111 ')
+  // const articles = await getBlogs('2b33b4bab64d4aeb9ee8f62aabe1ae5d')
+  // console.log(articles,'articles',blogsIds)
   const content = await getWorkSpace()
-  console.log(content,'content')
+  // console.log(content,'content')
   return <section className='w-full'>
      <header>
        <h1>My Blogs</h1>
+       {/* {JSON.stringify(results)} */}
      </header>
-    <div>
-      {JSON.stringify(content)}
-    </div>
      <div>
        <ArticleList articles={articles} />
        {/* {
