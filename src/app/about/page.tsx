3707c7ec93b0d6ac1c9984e6323b9da356744d9b
Mintnoii <MintnoiiQing@gmail.com>
2022-12-03 19:02:16 +0800

    fix: about fetch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,7 @@
-// import Image from 'next/image'
-'use client'
 import {getBlockContent, getPage } from '@/lib/notion'
-import { GetPageResponse, PageObjectResponse, PartialPageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
-
  export default async  function AboutPage() {
    let content = []
-   const page = await getPage(process.env.NOTION_ABOUT_PAGE_ID || '') as PageObjectResponse
+   const page = await getPage(process.env.NOTION_ABOUT_PAGE_ID || '')
  console.log('page', page)
   // const contents = await getBlockContent(page.id)
     let blocks = await getBlockContent(page.id)
