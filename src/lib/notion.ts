3f69880c51092e7d40bfd8274859c9cba52d6762
Mintnoii <MintnoiiQing@gmail.com>
2022-12-04 11:01:31 +0800

    wip: getAllBlockContent

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -2,25 +2,14 @@ import { Client } from '@notionhq/client'
 import * as R from 'remeda'
 import {getFormatDate} from '@/utils'
 import { IArticle } from '@/lib/types'
-import { PageObjectResponse, PartialDatabaseObjectResponse } from '@notionhq/client/build/src/api-endpoints'
-// import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
-// console.log(process.env.NOTION_TOKEN,'====', process.env.NOTION_DATABASE_ID)
+import { BlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
 const notion = new Client({ auth: process.env.NOTION_TOKEN })
 
-export const getDatabase = async (databaseId:string) => {
-  const response = await notion.databases.query({
-    // database_id: process.env.NOTION_DATABASE_ID || '',
-    database_id: databaseId,
-  })
-  return response
-}
+export const getDatabase = (database_id:string) => notion.databases.query({ database_id })
 
-export const getPage = async (pageId:string) => {
-  const response = await notion.pages.retrieve({ page_id: pageId })
-  return response
-}
+export const getPage = (page_id:string) => notion.pages.retrieve({ page_id })
 
-export const getBlockContent = async (block_id:string, start_cursor?:string|null) => {
+export const getBlocks = async (block_id:string, start_cursor?:string|null) => {
   const response = await notion.blocks.children.list({
     block_id,
     start_cursor: start_cursor || undefined,
@@ -29,6 +18,22 @@ export const getBlockContent = async (block_id:string, start_cursor?:string|null
   return response
 }
 
+ export const getAllBlockContent = async (page_id:string, start_cursor?:string|null) => {
+  const blocks = await getBlocks(page_id,start_cursor)
+  const content = [...blocks.results]
+  if (blocks.has_more) {
+    const nextBlocks = await getAllBlockContent(page_id, blocks.next_cursor)
+    content.push(...nextBlocks)
+  }
+  return content
+}
+
+
+ export const fetchPage = async (page_id:string) => {
+  const page = await getPage(page_id)
+  const content = await getAllBlockContent(page_id)
+
+}
 
 export const getBlogs = async (databaseId:string):Promise<IArticle[]> => {
   const response = await notion.databases.query({
@@ -72,22 +77,33 @@ export const getWorkSpace = async () => {
   return response.results
 }
 
-// export const getPublishedArticles = async (databaseId) => {
-//   const response = await notion.databases.query({
-//     database_id: databaseId,
-//     filter: {
-//       property: 'Status',
-//       select: {
-//         equals: '✅ Published',
-//       },
-//     },
-//     sorts: [
-//       {
-//         property: 'Published',
-//         direction: 'descending',
-//       },
-//     ],
-//   })
+// export const transformPageToQuestion = (page: BlockObjectResponse ): any => {
+//   const data:any = {
+//     id: page.id,
+//     title: '',
+//     content: '',
+//     // Initialize other properties as needed
+//   };
 
-//   return response.results
-// }
+//   for (const key in page.properties) {
+//     const property = page.properties[key];
+//     switch (property.type) {
+//       case 'relation':
+//         data[key] = property.relation[0].id;
+//         break;
+
+//       case 'title':
+//       case 'rich_text':
+//         if (property[property.type].length > 0) {
+//           data[key] = property[property.type][0].text.content;
+//         }
+//         break;
+
+//       default:
+//         data[key] = property;
+//         break;
+//     }
+//   }
+
+//   return data;
+// };
