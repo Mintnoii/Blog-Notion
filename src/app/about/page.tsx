b8894c5754334974a13baf61f8e4689a2b9fe360
Mintnoii <MintnoiiQing@gmail.com>
2022-12-23 19:44:31 +0800

    wip: update code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,8 @@
 import {getPage } from '@/api'
-import {renderBlock} from '@/features/BlockRenderer'
+import {renderBlocks} from '@/features/BlockRenderer'
 
 export default async  function AboutPage() {
   const page = await getPage(process.env.NOTION_ABOUT_PAGE_ID || '')
-
-  const renderBlocks = (blocks:any) => {
-    return blocks.map((block:any) => (
-      <div key={block.id}>
-        {renderBlock(block)}
-      </div>
-    ))
-  }
-
   return (
     <div className='pt-8'>
       {/* {JSON.stringify(page)} */}
--- src/app/blog/[blogSlug]/page.tsx
+++ src/app/blog/[blogSlug]/page.tsx
@@ -1,16 +1,8 @@
 import {getPage } from '@/api'
-import {renderBlock} from '@/features/BlockRenderer'
+import {renderBlocks} from '@/features/BlockRenderer'
 
 const ArticlePage = async () => {
   const page = await getPage('fa134157-f87b-4a30-be57-12e13c1428df')
-
-  const renderBlocks = (blocks:any) => {
-    return blocks.map((block:any) => (
-      <div key={block.id} className='py-1 px-0.5'>
-        {renderBlock(block)}
-      </div>
-    ))
-  }
 
   return (
     <div>
