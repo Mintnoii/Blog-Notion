1381ddde7cce3199b24f63a27000f1c2a80dac1c
Mintnoii <MintnoiiQing@gmail.com>
2023-01-18 00:56:02 +0800

    feat: Error Page

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -2,4 +2,4 @@
 import { Link as NextUILink, LinkProps} from "@nextui-org/react"
 import NextLink from "next/link"
 
-export const Link = (props:LinkProps) => (<NextUILink {...props} as={NextLink} />)
+export const Link = (props:LinkProps) => (<NextUILink {...props} as={NextLink} data-focus-visible={false} />)
